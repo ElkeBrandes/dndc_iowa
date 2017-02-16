c31f942f29fcff661c74eee5dd66172c64e9bcd6
IASTATE\ebrandes <ebrandes@iastate.edu>
2017-02-16 13:23:43 -0600

    changed script to clip rasters of two swg int scenarios  to the whole Raccoon River Watershed

--- SubfieldDNDC05_clip_watershed.py
+++ SubfieldDNDC05_clip_watershed.py
@@ -1,8 +1,9 @@
-# in this script, the raster layer N_loss_reduction that includes the N loss reduction from
-# the status quo (2012-2015 in corn/soy) and the medium yielding (10,000 kg ha-1) switchgrass integration
-# (2006-2015, switchgrass in areas < US$ -150 ha-1 and > 60 kg ha-1) is clipped to
-# the North Racoon River watershed to be able to zoom into the pattern of subfield switchgrass areas
-# and reduction amounts
+# in this script, two raster layers are clipped to the North Raccoon River Watershed:
+# 1. nitrate leaching change under "tweak" scenario: converting areas losing > $ 100 and
+#    > 50 kg N through nitrate leaching
+# 2. nitrate leaching change under "nutrient reduction scenario": converting all areas losing
+#    money and > 50 kg N through nitrate leaching
+# we are assuming a switchgrass yield of 10,000 kg ha-1.
 
 print("Running script ...")
 print("")
@@ -18,16 +19,16 @@ arcpy.env.workspace = "C:\\Users\\ebrandes\\Documents\\ia_clumu\\ia_clumu.gdb"
 print("Importing watershed shapefile ...")
 print("")
 # import the required watershed shapefile into the file geodatabase
-in_features = "C:/Users/ebrandes/Documents/geodata/shapefiles/wbdhu12_a_07100006.shp"
+in_features = "C:/Users/ebrandes/Documents/geodata/shapefiles/WBD_HU_08_IA.shp"
 out_path = "C:\\Users\\ebrandes\\Documents\\ia_clumu\\ia_clumu.gdb"
-out_name = "HUC12_07100006"
+out_name = "Watersheds_HU_08_IA"
 arcpy.FeatureClassToFeatureClass_conversion(in_features, out_path, out_name)
 
 print("Reprojecting watershed feature class ...")
 print("")
 # reproject the feature class
-in_dataset = "HUC12_07100006"
-out_dataset = "HUC12_07100006_NAD83"
+in_dataset = out_name
+out_dataset = str(out_name) + "_NAD83"
 out_coor_system = arcpy.SpatialReference('NAD 1983 UTM Zone 15N')
 arcpy.Project_management(in_dataset, out_dataset, out_coor_system)
 
@@ -37,28 +38,26 @@ desc = arcpy.Describe(featureClass)
 spatialRef = desc.SpatialReference
 print("Just checking ... spatial reference system is " + str(spatialRef.Name) +".")
 
-print("Selecting impared watersheds from the North Racoon River and dissolving into one feature ...")
+print("Selecting impared watersheds from the North Raccoon River ...")
 print("")
 # make a feature layer from the HUC12 watershed selected from the watershed feature class
 in_features = out_dataset
-out_layer = "NorthRacoonRiver"
-where_clause = '"HUC12" IN' + "('071000060101', '071000060102', '071000060103', '071000060201', '071000060202', '071000060203', \
-'071000060204', '071000060205', '071000060206', '071000060207', '071000060208', '071000060301', '071000060302', \
-'071000060303', '071000060304', '071000060305', '071000060306', '071000060307', '071000060308', '071000060309', '071000060310', \
-'071000060403', '071000060801')"
+out_layer = "NorthRaccoonRiverWS"
+where_clause = "HUC_8" = 07100006
 arcpy.MakeFeatureLayer_management(in_features, out_layer, where_clause)
 
-# dissolve the features in the feature layer to one feature
-in_features = "NorthRacoonRiver"
-out_feature_class = "NorthRacoonRiverOne"
-arcpy.Dissolve_management(in_features, out_feature_class)
-
-print("Clipping N loss raster to watershed ...")
+print("Clipping N leaching reduction rasters to watershed ...")
 print("")
-# clip the subfield N loss reduction raster to the selected watershed boundaries
-in_raster = "N_loss_reduction"
-clip_features = "NorthRacoonRiverOne"
-out_raster = "N_loss_reduction_NRW"
+# clip the subfield nitrate leaching reduction raster 1 to the selected watershed boundaries
+in_raster = "NO3_leach_red_100_50" ##################### change to name of raster ####################
+clip_features = out_layer
+out_raster = "NO3_leach_red_100_50_NRRWS"
+arcpy.Clip_management(in_raster, "#", out_raster, clip_features, "#", "ClippingGeometry")
+
+# clip the subfield nitrate leaching reduction raster 2 to the selected watershed boundaries
+in_raster = "NO3_leach_red_0_50" ##################### change to name of raster ####################
+clip_features = out_layer
+out_raster = "NO3_leach_red_100_50_NRRWS"
 arcpy.Clip_management(in_raster, "#", out_raster, clip_features, "#", "ClippingGeometry")
 
 print("Importing water body and river feature classes ...")
@@ -107,14 +106,14 @@ print("Clipping water body and river feature classes to watershed boundaries ...
 print("")
 # clip feature classes to watershed boundaries
 in_features = "Waterbody_NAD83"
-clip_features = "NorthRacoonRiverOne"
-out_feature_class = "Waterbody_NRW"
+clip_features = out_layer
+out_feature_class = "Waterbody_NRRWS"
 arcpy.Clip_analysis(in_features, clip_features, out_feature_class)
 
 # clip feature classes to watershed boundaries
 in_features = "Rivers_NAD83"
-clip_features = "NorthRacoonRiverOne"
-out_feature_class = "Rivers_NRW"
+clip_features = out_layer
+out_feature_class = "Rivers_NRRWS"
 arcpy.Clip_analysis(in_features, clip_features, out_feature_class)
 
-print("All done!")
+print("All done! :)s")
